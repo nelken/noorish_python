c6231581a3f5f0db0a7c5bc87f833c0180120dc0
Rani <nelken@gmail.com>
2025-12-12 12:45:22 -0800

    add burnout.py, not using it yet

--- api/burnout.py
+++ api/burnout.py
@@ -7,9 +7,27 @@ from openai import OpenAI
 import os
 import dotenv
 
+try:
+    from api.conversation_state import ConversationState
+    from api.theme_state import ThemeState
+except ImportError:
+    from conversation_state import ConversationState
+    from theme_state import ThemeState
+
 ROOT_DIR = Path(__file__).resolve().parent.parent
 dotenv.load_dotenv(ROOT_DIR / ".env.local")
 dotenv.load_dotenv(ROOT_DIR / ".env")
 
-def get_burnout():
+with open('api/burnout-prompt.txt') as f:
+    burnout_prompt = f.read()
+
+def get_burnout(state: ConversationState, theme_state: ThemeState) -> str:
     pass
+
+def main():
+    get_burnout(None, None)
+
+
+
+if __name__ == '__main__':
+    main()
